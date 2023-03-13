d2ccf3944e7f7acf4edc399573d3d00791d24e73
Saperoxx <50676292+Saperoxx@users.noreply.github.com>
2023-03-13 12:16:32 +0100

    complete download script
     script  to download labels made by Polygon segmentation in app named LabelBox

--- download.py
+++ download.py
@@ -12,10 +12,9 @@ from tqdm import tqdm
 import cv2
 
 def download():
-    client = Client(api_key='eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiJjbGV4NWd3YTkwbzdhMDc0M2RjZXdmM3Z4Iiwib3JnYW5pemF0aW9uSWQiOiJjbGV4NWd3ODMwbzc5MDc0M2ZwMzg4dno2IiwiYXBpS2V5SWQiOiJjbGYzdHdiNW4xY3drMDgyczByb2VmaHgyIiwic2VjcmV0IjoiODI3MTIwZjc1YTVhNTFlMWY0NWQyY2YzNmE0YzEzYTAiLCJpYXQiOjE2Nzg1MzA3OTUsImV4cCI6MjMwOTY4Mjc5NX0.mQKPRfOSpgQ-uhd3vsKvQs7cZXE0A5mw5mtTBI3U54Y')
+    client = Client(api_key='LABEL_API_KEY')
 
-    project = client.get_project('clezrgpm711mf07w63ev175kn')
-    # project = client.get_project('clf3vdjfi0kjc073thlk7as7c')
+    project = client.get_project('PROJECT_ID')
     labels = project.label_generator()
 
     rgb_dir = Path('./data/sunflowers/images/')
@@ -46,31 +45,46 @@ def download_image(url: str, output_path: Path):
     image = io.imread(url)
     io.imsave(str(output_path), image)
 
+def is_ripe(mask):
+    return str(mask['value']) == 'ripe_sunflower'
+
+def is_ripe_85(mask):
+    return str(mask['value']) == 'ripe_sunflower_85'
+
+def is_unripe(mask):
+    return str(mask['value']) == 'unripe_sunflower'
 
 def download_annotations(annotations: List[Any], output_path: Path):
+    #colors
+    white = np.uint8((255, 255, 255))
+    red = np.uint8((255, 0, 0))
+    blue = np.uint8((0, 0, 255))
+    green = np.uint8((0, 255, 0))
+    orange = np.uint8((255, 100, 0))
+
     mask_data = None
+
     for annotation in annotations:
         if isinstance(annotation.extra, Radio):
             continue
 
         mask: Mask = annotation.extra
-        # mask: Mask = annotation.value
-        # annotation_data = io.imread(mask.mask.url)[..., :3]
         annotation_data = io.imread(mask['instanceURI'])[..., :3]
-        print(mask)
-        # if str(mask['value']) == 'ripe_sunflower':
         mask_data = np.zeros_like(annotation_data) if mask_data is None else mask_data
-        mask_data = np.where(annotation_data == (255, 255, 255), np.uint8((0, 255, 0)), mask_data)
-        # elif str(mask['value']) == 'ripe_sunflower_85%':
-        #     mask_data = np.zeros_like(annotation_data) if mask_data is None else mask_data
-        #     mask_data = np.where(annotation_data == (240, 180, 0), np.uint8((0, 255, 0)), mask_data)
-        # elif str(mask['value']) == 'unripe_sunflower':
-        #     print('good')
-        #     mask_data = np.zeros_like(annotation_data) if mask_data is None else mask_data
-        #     mask_data = np.where(annotation_data == (255, 0, 0), np.uint8((0, 255, 0)), mask_data)
+
+        if is_ripe(mask):
+            mask_data = np.where(annotation_data == white, green, mask_data)
+
+        if is_ripe_85(mask):
+            mask_data = np.where(annotation_data == white, orange, mask_data)
+
+        if is_unripe(mask):
+            mask_data = np.where(annotation_data == white, red, mask_data)
 
     if mask_data is not None:
         io.imsave(str(output_path), mask_data, check_contrast=False)
+
+
 
 
 if __name__ == '__main__':
